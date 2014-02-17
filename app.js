d06e8804a0b6e7829d393142b9e99d245edc6218
shashidhar <shasidhar@zinniasystems.com>
2014-02-17 15:32:35 +0530

    removed scala classes, kept only html files

---
+++ app.js
@@ -0,0 +1,18 @@
+function Login()
+	{
+		FB.login(function(response) {
+		   if (response.authResponse)
+		   {
+		   	    var loginButton = document.getElementById('loginButton');
+		   	    loginButton.parentNode.removeChild(loginButton);
+		   	    var message_span = document.getElementById('topMessageSpan');
+		   	    message_span.innerHTML="Fetching friends information.....";
+                console.log("User authorised")
+
+  			} else
+  			{
+  	    	 console.log('User cancelled login or did not fully authorize.');
+   			}
+		 },{scope: 'email,user_friends'});
+
+ }
