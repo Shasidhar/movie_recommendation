b10b75f3340658757d6f989693927fe65764bd18
Shashidhar E S <shasidhar@zinniasystems.com>
2014-02-18 17:39:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function Login()
   {
     FB.login(function(response) {
@@ -19,9 +18,12 @@ function Login()
  }
 function getAllMoviesOfUser(id) {
   FB.api('/'+id+'/movies/?fields=name,picture.width(100).height(100),link', function(response) {
-    console.log(response.length);
-    message_span.innerHTML=response.data.length+" Movies you like";
-         for(j=0;j<response.length;j++)
+    console.log(response.movies.length);
+     if(totalCount>0) {
+      	var message_span = document.getElementById('topMessageSpan');
+      	message_span.innerHTML=response.data.length+" Movies you like";
+     }
+    for(j=0;j<response.movies.length;j++)
         {
           console.log(j);
           for(k=0;k<response[j].data.length;k++){
