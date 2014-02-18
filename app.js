b51fe1a2a729ace98923da10aaa32dee31be76df
shashidhar <shasidhar@zinniasystems.com>
2014-02-18 14:02:11 +0530

    images fetched

--- app.js
+++ app.js
@@ -19,21 +19,25 @@ function Login()
 
  }
 function getAllMoviesOfUser(id) {
-  FB.api('/'+id+'/movies/?fields=picture.width(100).height(100),link', function(response) {
+  FB.api('/'+id+'/movies/?fields=name,picture.width(100).height(100),link', function(response) {
   for(i=0;i<response.data.length;i++){
          movies.push(response.data[i])
         }
       })
    FB.api('/'+id+'/friends', function(response) {
     for(i=0;i<response.data.length;i++){
-        console.log(response.data[i])
          friends.push(response.data[i])
         }
       })
 
 for(k=0;k<friends.length;k++){
-    FB.api('/'+friends[k].id+'/movies?fields=movies,picture.width(100).height(100),link', function(response) {
-    console.log(response.data)
+    FB.api('/'+friends[k].id+'/movies?fields=name,picture.width(100).height(100),link', function(response) {
+    if(response.data.length>0){
+    for(i=0;i<response.data.length;i++){
+      console.log(response.data[i])
+         movies.push(response.data[i])
+        }
+      }
     }) 
   }
-} 
+}
