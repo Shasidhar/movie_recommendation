6bdaeec5dae36c7842625fead7a86d95436e5d97
Shashidhar E S <shasidhar@zinniasystems.com>
2014-02-18 15:46:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ function getAllMoviesOfUser(id) {
    getAllMovies(friends)*/
    FB.api('/'+id+'/friends?fields=movies', function(response) {
    for(i=0;i<response.data.length;i++){
-         friendMovies.push(response)
+         friendMovies.push(response.data[i])
        }
      })
 
