23c8194bf236b008465a686ab1787c20034b49b5
shashidhar <shasidhar@zinniasystems.com>
2014-02-18 11:17:28 +0530

    app.js modified

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+var movies = new Array(); 
 function Login()
 	{
 		FB.login(function(response) {
 		   if (response.authResponse)
 		   {
-            var movies = new Array(); 
 		   	    var loginButton = document.getElementById('loginButton');
 		   	    loginButton.parentNode.removeChild(loginButton);
 		   	    var message_span = document.getElementById('topMessageSpan');
 		   	    message_span.innerHTML="Movies liked by You and Your friends";
             console.log(response.authResponse.userID)
             getAllMoviesOfUser(response.authResponse.userID,movies)
-            console.log(movies)
+            console.log(Movies)
   			} else
   			{
   	    	 console.log('User cancelled login or did not fully authorize.');
