506ce20b2b102c9f4785db454140b7ce7e159087
shashidhar <shasidhar@zinniasystems.com>
2014-02-18 11:16:20 +0530

    app.js modified

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function Login()
 		   	    message_span.innerHTML="Movies liked by You and Your friends";
             console.log(response.authResponse.userID)
             getAllMoviesOfUser(response.authResponse.userID,movies)
-
+            console.log(movies)
   			} else
   			{
   	    	 console.log('User cancelled login or did not fully authorize.');
