4f13311c2fe57316635a16007e099e03f65aa9e8
Shashidhar E S <shasidhar@zinniasystems.com>
2014-02-18 18:38:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,12 +44,31 @@ function getAllMoviesOfUser(id) {
             document.getElementById('friends').appendChild(newDiv);
      }
   })}
+
 function getAllMoviesOfUserFriends(id) {
   FB.api('/'+id+'/friends?fields=movies.fields(name,link,picture.width(100).height(100))', function(response) {
     var totalCount = 0;
     for(i=0;i<response.data.length;i++){
      if(response.data[i].movies!=undefined){
-         console.log(response.data[i].movies)
+         var userMovies = response.data[i].movies);
+          for(j=0;j<userMovies.data.length;j++){
+
+            var name=userMovies.data[j].name;
+               var picture_url = userMovies.data[j].picture.data.url;
+               var link = userMovies.data[j].link
+               var newDiv = document.createElement('div');
+               newDiv.className="friendDiv col-md-1 col-xs-4"              
+               var profileLink = document.createElement('a')
+               profileLink.href=link;
+               profileLink.target="_blank"
+               var img = document.createElement('img');
+               img.title=name;
+               img.src=picture_url;
+               img.className="img-responsive";
+               profileLink.appendChild(img);
+               newDiv.appendChild(profileLink);                       
+            document.getElementById('friends').appendChild(newDiv);
+          }
         }  
        }    
 })
