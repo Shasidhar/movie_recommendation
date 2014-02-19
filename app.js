ee9dd9a684eda4e4c78f1a26cb0c9319bc6771d9
Shashidhar E S <shasidhar@zinniasystems.com>
2014-02-19 13:07:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,29 +52,10 @@ unique = function( arr ){
 function getOnlyEnglishMovies(){
   console.log(allMovies[10].name);
   var uniqueMovies = unique(allMovies);
-  /*var movies = allMovies.map(function(obj) { return obj.id; });
-  movies = movies.filter(function(v,i) { return movies.indexOf(v) == i; });*/
-  var messagespan = document.getElementById('topMessageSpan');
-  messagespan.innerHTML="Total movies liked by you and your friends are "+uniqueMovies.length;
-  var validMovies = new Array();
-  for(i=0;i<uniqueMovies.length;i++){
-      var movie = JSON.parse(uniqueMovies[i]);
-      validMovies.push(movie);
-      var name=movie.name;
-               var picture_url = movie.picture.data.url;
-               var link = movie.link
-               var newDiv = document.createElement('div');
-               newDiv.className="friendDiv col-md-1 col-xs-4"              
-               var profileLink = document.createElement('a')
-               profileLink.href=link;
-               profileLink.target="_blank"
-               var img = document.createElement('img');
-               img.title=name;
-               img.src=picture_url;
-               img.className="img-responsive";
-               profileLink.appendChild(img);
-               newDiv.appendChild(profileLink);                       
-            document.getElementById('friends').appendChild(newDiv);
+  var uniqueMovieObjects = uniqueMovies.map(function(obj) { return JSON.parse(obj); });
+  console.log(uniqueMovieObjects[0]);
+ // movies = movies.filter(function(v,i) { return movies.indexOf(v) == i; });
+  
             
 }
 }
