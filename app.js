d0da5a12ba99c465c42b6fcf79aa95d308d83add
Shashidhar E S <shasidhar@zinniasystems.com>
2014-02-28 13:22:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ function getAllMoviesOfUserFriends(id) {
   FB.api('/'+id+'/friends?fields=movies.fields(name,link,picture.width(100).height(100))',null,null, function(response) {
     var totalCount = 0;
     for(i=0;i<response.data.length;i++){
-     if(response.data[i].movies!=undefirined){
+     if(response.data[i].movies!=undefined){
          var userMovies = response.data[i].movies;
           for(j=0;j<userMovies.data.length;j++){
             allMovies.push(userMovies.data[j]);
@@ -80,7 +80,7 @@ var service_url = 'https://www.googleapis.com/freebase/v1/mqlread?query=';
                 	img.className="img-responsive";
                		profileLink.appendChild(img);
                 	newDiv.appendChild(profileLink);                       
-            		document.getElementById('moviesforyou').appendChild(newDiv);	
+            		document.getElementById('friends').appendChild(newDiv);	
  			};
  		}
  		});
