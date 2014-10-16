68ea1db2030b1f94a4e38e53946c768b3dac36f8
Shashidhar E S <shasidhar@zinniasystems.com>
2014-10-16 16:56:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ function getAllMoviesOfUser(id) {
       var movie = response.data[i];
             allMovies.push(movie);
      }
-      populateMovies();
+      //populateMovies();
   })}
 
 function getAllMoviesOfUserFriends(id) {
-  FB.api('/'+id+'/friends?fields=movies.fields(name,link,picture)',null,null, function(response) {
+  FB.api('/'+id+'/friends?fields=movies.fields(name,link)',null,null, function(response) {
     var totalCount = 0;
     for(i=0;i<response.data.length;i++){
      if(response.data[i].movies!=undefined){
@@ -40,7 +40,7 @@ function getAllMoviesOfUserFriends(id) {
           }
         }  
        }
-       //populateMovies();
+       populateMovies();
 })
 }
 unique = function( arr ){
